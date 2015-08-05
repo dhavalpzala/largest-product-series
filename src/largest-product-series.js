99c72c2109f7854398491df52ac3b2011cab5330
dhavalsinhzala <dhaval.zala@live.com>
2015-08-05 12:33:04 +0530

    resolved major issues

--- src/largest-product-series.js
+++ src/largest-product-series.js
@@ -1,21 +1,15 @@
 (function() {
     var app = angular.module("largestProductSeries", []);
     app.controller("MainController", [ '$scope', '$sce', function($scope, $sce) {
-        $scope.findSeries = function(inputString,
-                length) {
+        $scope.findSeries = function(inputString, length) {
 
-            var index, result = -1, tempResult = 0, loopLength = inputString.length
-            - length + 1, selectedIndex = -1;
+            var index, result = -1, tempResult = 0, loopLength = inputString.length - length + 1, selectedIndex = -1, seriesMultiplicationResult;
             if (!validateInputs(inputString, length)) {
-                setResult.call(this, inputString,
-                        length, -1, "");
+                setResult.call(this, inputString, length, -1, "");
                 return;
             }
             for (index = 0; index < loopLength; index++) {
-                var digit, seriesMultiplicationResult;
-                seriesMultiplicationResult = calculateSeriesMultiplication(
-                        tempResult, inputString,
-                        length, index);
+                seriesMultiplicationResult = calculateSeriesMultiplication(tempResult, inputString, length, index);
                 index = seriesMultiplicationResult.index;
                 tempResult = seriesMultiplicationResult.tempResult;
                 if (tempResult > result) {
@@ -24,16 +18,14 @@
                 }
             }
 
-            setResult.call(this, inputString, length,
-                    selectedIndex, result);
+            setResult.call(this, inputString, length, selectedIndex, result);
         };
-        function calculateSeriesMultiplication(
-                tempResult, inputString, length, index) {
+        function calculateSeriesMultiplication(tempResult, inputString, length, index) {
+            var digit;
             if (!tempResult) {
                 tempResult = 1;
                 for (var innerIndex = 0; innerIndex < length; innerIndex++) {
-                    digit = inputString.charAt(index
-                            + innerIndex);
+                    digit = inputString.charAt(index + innerIndex);
                     if (digit === '0') {
                         index = index + innerIndex;
                         tempResult = 0;
@@ -45,8 +37,7 @@
             } else {
                 digit = inputString.charAt(index - 1);
                 tempResult /= digit;
-                digit = inputString.charAt(index
-                        + length - 1);
+                digit = inputString.charAt(index + length - 1);
                 if (digit === '0') {
                     index = index + length - 1;
                     tempResult = 0;
@@ -56,38 +47,21 @@
             }
 
             return {
-                index : index,
-                tempResult : tempResult
+                index : index, tempResult : tempResult
             };
         }
         function validateInputs(inputString, length) {
-            return inputString
-            && length
-            && new RegExp("^[0-9]*$")
-            .test(inputString);
+            return inputString && length && new RegExp("^[0-9]*$").test(inputString);
         }
         function setResult(inputString, length,
                 selectedIndex, result) {
             if (selectedIndex >= 0) {
                 this.result = result;
-                this.resultString = $sce
-                .trustAsHtml(inputString
-                        .substring(0,
-                                selectedIndex)
-                                + "<span class='red'> "
-                                + inputString
-                                .substring(
-                                        selectedIndex,
-                                        selectedIndex
-                                        + length)
-                                        + "</span>"
-                                        + inputString
-                                        .substring(selectedIndex
-                                                + length));
+                this.resultString = $sce.trustAsHtml(inputString.substring(0, selectedIndex) + "<span class='red'> "
+                                + inputString.substring(selectedIndex, selectedIndex + length) + "</span>" + inputString.substring(selectedIndex + length));
             } else {
                 this.result = "";
-                this.resultString = $sce
-                .trustAsHtml("<span class='red'>please enter valid inputs</span>");
+                this.resultString = $sce.trustAsHtml("<span class='red'>please enter valid inputs</span>");
             }
         }
     } ]);
